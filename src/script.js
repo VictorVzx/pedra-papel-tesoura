d981f471f575a7af648aade7f08590ba698647cd
Victor Compertino <gifpa45we@gmail.com>
2025-12-29 00:39:33 -0300

    (script.js) melhorar a lógica do jogo

--- src/script.js
+++ src/script.js
@@ -1,40 +1,30 @@
 const btnJg = document.querySelector('.botaoJogar');
 const msg = document.querySelector('.message')
 
-const pedra = "pedra".toLowerCase().trim()
-const papel = "papel".toLowerCase().trim()
-const tesoura = "tesoura".toLowerCase().trim()
+const jogadasValidas = ["pedra", "papel", "tesoura"];
 
 btnJg.addEventListener("click", () => {
     const p1 = document.getElementById('jogador1').value.toLowerCase().trim();
     const p2 = document.getElementById('jogador2').value.toLowerCase().trim();
 
-    if (p1 == tesoura && p2 == papel){
-        msg.textContent = "Jogador 1 venceu!";
+    if(p1 === "" || p2 === ""){
+        msg.textContent = "Os campos não podem estar vazios!"
     }
-    else if(p1 == papel && p2 == tesoura){
-        msg.textContent = "Jogador 2 venceu!"
+    else if (!jogadasValidas.includes(p1) || !jogadasValidas.includes(p2)) {
+        msg.textContent = "Caractere desconhecido!"
     }
-    else if(p1 == papel && p2 == pedra){
-        msg.textContent = "Jogador 1 venceu!"
-    }
-    else if(p1 == pedra && p2 == papel){
-        msg.textContent = "Jogador 2 venceu!"
-    }
-    else if(p1 == pedra && p2 == tesoura){
-        msg.textContent = "Jogador 1 venceu!"
-    }
-    else if(p1 == tesoura && p2 == pedra){
-        msg.textContent = "Jogador 2 venceu!"
-    }
-    else if (p1 == "" || p2 == ""){
-        msg.textContent = "Os campos não podem estar vazios."
-    }
-    else if(p1 === p2) {
+    else if (p1 === p2){
         msg.textContent = "Empate!"
     }
+    else if (
+        (p1 === "tesoura" && p2 === "papel") ||
+        (p1 === "papel" && p2 === "pedra") ||
+        (p1 === "pedra" && p2 === "tesoura")
+    ) {
+        msg.textContent = "Jogador 1 venceu!"
+    }
     else {
-        msg.textContent = "Elemento inválido!"
+        msg.textContent = "Jogador 2 venceu!"
     }
 
     setTimeout(() => {
